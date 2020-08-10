d61e9d2828d9b0fdb92a40faaa3e315bce9dbf16
Andrei-98 <65445007+Andrei-98@users.noreply.github.com>
2020-08-10 12:49:19 +0200

    Support for "+"
    Program is not able to add two matrices together and print the result.

--- processor.py
+++ processor.py
@@ -7,4 +7,14 @@ def get_matrix():
 list_one = get_matrix()
 list_two = get_matrix()
 
-print(list_one, list_two)
+if len(list_one) != len(list_two) or len(list_one[0]) != len(list_two[1]):
+    print("ERROR")
+else:
+    together = []
+    for i in range(len(list_one)):
+        together.append([])
+        for j in range(len(list_one[i])):
+            together[i].append(str(int(list_one[i][j]) + int(list_two[i][j])))
+
+    for i in range(len(together)):
+        print(" ".join(together[i]))
